4cd28c921cb7f855f740dfab60edc9c6299a4e57
David Vezzani <david_vezzani@yahoo.com>
2016-06-30 08:51:11 -0700

    make companies functional

--- lib/controllers/api/v1/companies.rb
+++ lib/controllers/api/v1/companies.rb
@@ -4,7 +4,21 @@ module AwesomeCompany
   module API
     module V1
       class Companies < Grape::API
+      	params do
+      	  optional :name, type: String, desc: 'Company name you wish to filter on'
+      	end
+      	resource :companies do
+      	  desc "Retrieve a list of Companies"
+      	  get do
+      	  	if params[:name]
+      	  	  Company.where(name: params[:name])
+      	  	else
+      	  	  Company.all
+      	  	end  
+      	  end
+      	end
       end
     end
   end
 end  
+
